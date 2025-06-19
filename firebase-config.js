5857594c8f9477804281d36fec978444fd9d8c06
Riku-tatuto <175525675+Riku-tatuto@users.noreply.github.com>
2025-06-19 18:36:50 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebaseのプロジェクト設定から取得
+const firebaseConfig = {
+  apiKey: "AIzaSyCQdnFMnR4UKZRjTYJYgM98StDklLssZOA",
+  authDomain: "online-chat-app-db.firebaseapp.com",
+  databaseURL: "https://online-chat-app-db-default-rtdb.firebaseio.com",
+  projectId: "online-chat-app-db",
+  storageBucket: "online-chat-app-db.firebasestorage.app",
+  messagingSenderId: "485166356040",
+  appId: "1:485166356040:web:1071fed931b12a9197aa7d"
+};
+
+// Firebase初期化
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database = firebase.database();
